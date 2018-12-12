5b0c1a1189fd733ad8400f3a3a7a63b9232f448f
Jeff Goergen <danthestickman@yahoo.com>
2018-12-12 00:00:51 -0500

    updated light detector, tweaks settings

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,19 +1,20 @@
+
 #include <tinySPI.h> // https://github.com/JChristensen/tinySPI
 
 #define LATCH_PIN  3 // 74HC595 - ST_CP / 12
 #define DATA_PIN   1 // 74HC595 - DS / 14
 #define CLOCK_PIN  2 // 74HC595 - SH_CP / 11
 #define OUTPUT_ENABLE_PIN     0 // 74HC595 - OE / 13
-#define LIGHT_DETECTOR_PIN    4 // light detecting resistor to positive pin on attiny85
+#define LIGHT_DETECTOR_PIN    A2 // light detecting resistor to positive pin on attiny85
 
 // SETTINGS ////////
-int maxLoopDelay = 20;
+int maxLoopDelay = 40;
 int minLoopDelay = 4;
-int maxPWMFreq = 255;
+int maxPWMFreq = 130;
 int minPWMFreq = 0;
 int maxWaitBetweenLEDS = 6000;
-int maxLightsAtATime = 2;
-int minLightBeforeShutoff = 400; // out of 1024
+int maxLightsAtATime = 1;
+int minLightBeforeShutoff = 300; // out of 1024, point at which the light turns off
 //////////////////////
 
 int pwmFreq = 123;
@@ -25,7 +26,8 @@ int lightCount = 1;
 void setup() {
   
   randomSeed(analogRead(4));
-  pinMode(OUTPUT_ENABLE_PIN, OUTPUT);
+  pinMode(OUTPUT_ENABLE_PIN, OUTPUT);  
+  pinMode(LIGHT_DETECTOR_PIN, INPUT);
   SPI.begin();
   pinMode(LATCH_PIN, OUTPUT); 
   digitalWrite(LATCH_PIN, HIGH);
@@ -35,12 +37,12 @@ void setup() {
 void loop() {
 
   // if there's too much light, stop it
-  int lightVal = analogRead(LIGHT_DETECTOR_PIN);
-  if (lightVal > minLightBeforeShutoff) {
+  if (analogRead(LIGHT_DETECTOR_PIN) > minLightBeforeShutoff) {
 
     digitalWrite(LATCH_PIN, LOW);
     SPI.transfer(0b00000000);
     digitalWrite(LATCH_PIN, HIGH);
+    analogWrite(OUTPUT_ENABLE_PIN, 0);
     delay(1000);
     return;
   }
@@ -55,7 +57,14 @@ void loop() {
 void updatePWM() {
 
   pwmFreq += pwmFreqChangeSpeed;
-  int easedPWM = (int)easeInOutQuad((float)pwmFreq, (float)minPWMFreq, (float)(maxPWMFreq - minPWMFreq), (float)maxPWMFreq);
+  
+  int easedPWM = 
+    (int)easeInOutQuad(
+      (float)pwmFreq, // current time
+      (float)minPWMFreq, // begInnIng value
+      (float)(maxPWMFreq - minPWMFreq), // change In value
+      (float)maxPWMFreq); // duration
+      
   analogWrite(OUTPUT_ENABLE_PIN, easedPWM);
 
   if (pwmFreq >= maxPWMFreq) {
