407c6c19da43e7fdb1179056214535920a2a2021
Jeff Goergen <jeffg@wambatech.com>
2018-12-20 15:29:28 -0500

    improved readability

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,43 +1,55 @@
+// Don't forget to add the ATTINY85 board to your ide. You'll need to add this to your preferences -> additional board manager urls
+// https://raw.githubusercontent.com/damellis/attiny/ide-1.6.x-boards-manager/package_damellis_attiny_index.json
+// More info on working with the ATTINY85 here: http://highlowtech.org/?p=1695
 
 #include <tinySPI.h> // https://github.com/JChristensen/tinySPI
 
-#define LATCH_PIN  3 // 74HC595 - ST_CP / 12
-#define DATA_PIN   1 // 74HC595 - DS / 14
-#define CLOCK_PIN  2 // 74HC595 - SH_CP / 11
-#define OUTPUT_ENABLE_PIN     0 // 74HC595 - OE / 13
-#define LIGHT_DETECTOR_PIN    A2 // light detecting resistor to positive pin on attiny85
+// PINS ////////////////////////////////////////////////////////////////////
 
-// SETTINGS ////////
-int maxLoopDelay = 40;
-int minLoopDelay = 4;
-int maxPWMFreq = 255;
-int minPWMFreq = 100;
-int maxWaitBetweenLEDS = 6000;
-int maxLightsAtATime = 1;
-int minLightBeforeShutoff = 40; // out of 1024, beyond this point the light turns off
-//////////////////////
+#define LATCH_PIN               3 // 74HC595 - ST_CP / 12
+#define DATA_PIN                1 // 74HC595 - DS / 14
+#define CLOCK_PIN               2 // 74HC595 - SH_CP / 11
+#define OUTPUT_ENABLE_PIN       0 // 74HC595 - OE / 13
+#define LIGHT_DETECTOR_PIN      A2 // light detecting resistor to positive pin on attiny85 ( neesd a resistor from ground to A2 as well. )
+
+// SETTINGS ////////////////////////////////////////////////////////////////
+
+#define MAX_LOOP_DELAY_SPEED    40 // the maximum speed ( picked at random between min and ma ) to fade the leds
+#define MIN_LOOP_DELAY_SPEED    4 // the minimum speed ( picked at random between min and max ) to fade the leds
+#define MAX_BRIGHTNESS_PWM      255 // the minimum brightness of the leds when faded all the way out ( inverted, so 255 - this value )
+#define MIN_BRIGHTNESS_PWM      100 // the maximum brightness of the leds when faded all the way in ( inverted, so 255 - this value )
+#define MAX_WAIT_BETWEEN_LEDS   6000 // the maximum time to wait between lighting leds
+#define MAX_LIGHTS_AT_A_TIME    1 // the maximum amount of leds that will be lit up at a time
+#define LIGHT_COUNT             8 // the amouunt of lights connected
+#define USE_LIGHT_SENSOR        0 // 1 to use a photoressitor to turn it off when some amount of ambient light is detected
+#define MIN_LIGHT_BEFORE_DISABLE 40 // out of 1024, beyond this point the light turns off
+
+// VARIABLES ////////////////////////////////////////////////////////////////
 
 int pwmFreq = 123;
 int pwmFreqChangeSpeed = 1;
 uint8_t currentLEDS = 0b0001000;
-int loopDelay = minLoopDelay;
+int loopDelay = MIN_LOOP_DELAY_SPEED;
 int lightCount = 1;
 
 void setup() {
   
   randomSeed(analogRead(4));
   pinMode(OUTPUT_ENABLE_PIN, OUTPUT);  
-  pinMode(LIGHT_DETECTOR_PIN, INPUT);
   SPI.begin();
   pinMode(LATCH_PIN, OUTPUT); 
   digitalWrite(LATCH_PIN, HIGH);
+
+  if (USE_LIGHT_SENSOR == 1)
+    pinMode(LIGHT_DETECTOR_PIN, INPUT);
+  
   changeLED();
 }
 
 void loop() {
 
   // if there's too much light, stop it
-  if (analogRead(LIGHT_DETECTOR_PIN) > minLightBeforeShutoff) {
+  if (USE_LIGHT_SENSOR == 1 && analogRead(LIGHT_DETECTOR_PIN) > MIN_LIGHT_BEFORE_DISABLE) {
 
     digitalWrite(LATCH_PIN, LOW);
     SPI.transfer(0b00000000);
@@ -61,18 +73,18 @@ void updatePWM() {
   int easedPWM = 
     (int)easeInOutQuad(
       (float)pwmFreq, // current time
-      (float)minPWMFreq, // begInnIng value
-      (float)(maxPWMFreq - minPWMFreq), // change In value
-      (float)maxPWMFreq); // duration
+      (float)MIN_BRIGHTNESS_PWM, // begInnIng value
+      (float)(MAX_BRIGHTNESS_PWM - MIN_BRIGHTNESS_PWM), // change In value
+      (float)MAX_BRIGHTNESS_PWM); // duration
       
   analogWrite(OUTPUT_ENABLE_PIN, easedPWM);
 
-  if (pwmFreq >= maxPWMFreq) {
+  if (pwmFreq >= MAX_BRIGHTNESS_PWM) {
 
     pwmFreqChangeSpeed *= -1;    
     changeLED();   
     
-  } else if (pwmFreq <= minPWMFreq) {
+  } else if (pwmFreq <= MIN_BRIGHTNESS_PWM) {
 
     pwmFreqChangeSpeed *= -1; 
   }  
@@ -80,14 +92,14 @@ void updatePWM() {
 
 void changeLED() {
   
-  lightCount = random(maxLightsAtATime) + 1;
+  lightCount = random(MAX_LIGHTS_AT_A_TIME) + 1;
   currentLEDS = 0b00000000;
   
   for (int i = 0; i < lightCount; i++)
-    currentLEDS = currentLEDS | 1 << random(8);
+    currentLEDS = currentLEDS | 1 << random(LIGHT_COUNT);
   
-  loopDelay = minLoopDelay + random(maxLoopDelay);
-  delay(random(maxWaitBetweenLEDS));
+  loopDelay = MIN_LOOP_DELAY_SPEED + random(MAX_LOOP_DELAY_SPEED);
+  delay(random(MAX_WAIT_BETWEEN_LEDS));
 }
 
 // t: current time, b: begInnIng value, c: change In value, d: duration
